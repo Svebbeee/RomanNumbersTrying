23ad3e9c84498b89b1f0a7a533ec03d636d884f8
Svebbeee <sandlandsverre@gmail.com>
2021-09-08 12:32:54 +0200

    Scanner

--- src/main/java/no/kristiania/RomanNumbers.java
+++ src/main/java/no/kristiania/RomanNumbers.java
@@ -1,4 +1,13 @@
 package no.kristiania;
 
+import java.util.Scanner;
+
 public class RomanNumbers {
+    public static void main(String[] args) {
+        System.out.println("Which number do you want to convert to roman?");
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println(input);
+    }
 }
