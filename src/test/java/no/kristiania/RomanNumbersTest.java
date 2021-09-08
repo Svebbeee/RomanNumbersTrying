88b824d6125afb318678091fabb607a82f7870dc
Svebbeee <sandlandsverre@gmail.com>
2021-09-08 12:17:50 +0200

    Initial commit

---
+++ src/test/java/no/kristiania/RomanNumbersTest.java
@@ -0,0 +1,12 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumbersTest {
+
+    @Test
+    void DummyTest(){
+        assertEquals(42, 6*7);
+    }
+}
